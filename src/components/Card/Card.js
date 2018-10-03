5eaeb6729c99f77cee552d17bf460772871a02bd
ptaiton <pierre.taiton@outlook.fr>
2018-10-03 18:05:43 +0200

    Cards improvements

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,42 +2,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
   card: {
-    maxWidth: 345,
+    maxWidth: 500,
+    maxHeight: 500,
+    minWidth: 500,
+    minHeight: 500
   },
   media: {
     objectFit: 'cover',
   },
+  positionImageCard: {
+    margin: '20px 0px 20px 0px',
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  title: {
+    fontFamily:'Roboto, sans-serif',
+  },
+  text: {
+    fontFamily:'Montserrat, sans-serif',
+  }
 };
 
 function ImgMediaCard(props) {
   const { classes, image, title , description } = props;
   return (
-    <Card className={classes.card}>
-      <CardActionArea>
+    <Card className={`${classes.card} ${classes.positionImageCard}`}>
         <CardMedia
           component="img"
           alt="Contemplative Reptile"
-          className={classes.media}
-          height="140"
+          className={`${classes.media}`}
+          height="170"
           image={image}
           title="Contemplative Reptile"
         />
         <CardContent>
-          <Typography gutterBottom variant="headline" component="h2">
+          <Typography gutterBottom variant="headline" component="h2" className={`${classes.title}`}>
             {title}
           </Typography>
-          <Typography component="p">
-            {description}
+          <Typography component="p" className={`${classes.text}`}>
+            <div>
+              {description.split('\n\n').map(line => (
+                <span> {line} <br /></span>
+              ))}
+            </div>
           </Typography>
         </CardContent>
-      </CardActionArea>
     </Card>
   );
 }
